a3d98857b3f89fd14f9538b1317508bc1884820e
Amr-Ezz <amroelghitany@gmail.com>
2023-06-15 16:42:29 +0300

    Billing Business Button CardDeal updated

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Button = () => {
-  return (
-    <div>
+const Button = ({styles}) => (
+  
+    <button type="button" className={`px-6 py-4 font-poppins font-medium text-[19px] bg-blue-gradient rounded-[10px] text-primary outline-none ${styles}`}>
+      Get Started
       
-    </div>
-  )
-}
+    </button>
+  
+)
 
 export default Button
